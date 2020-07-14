2a25d09920929778b5d6b7584bebcbd8edb963a3
mike-bennett <michael.t.bennett14@gmail.com>
2020-07-13 22:28:04 -0400

    Use tibbles instead of data.frames

--- run_analysis.R
+++ run_analysis.R
@@ -2,36 +2,37 @@ library(tidyverse)
 options(digits = 7)
 
 ## Read in X test, train data and bind rows
-train_x <- read.csv("Data/train/X_train.txt", sep = " ", header = FALSE)
-test_x <- read.csv("Data/test/X_test.txt", sep = " ", header = FALSE)
+train_x <- read_delim("Data/train/X_train.txt", delim = " ", col_names = FALSE)
+test_x <- read_delim("Data/test/X_test.txt", delim = " ", col_names = FALSE)
 x_data <- rbind(train_x, test_x)
 
+
 ## Read in Y test, train data and bind rows
-train_y <- read.csv("Data/train/Y_train.txt", sep = " ", header = FALSE)
-test_y <- read.csv("Data/test/Y_test.txt", sep = " ", header = FALSE)
+train_y <- read_delim("Data/train/Y_train.txt", delim = " ", col_names = FALSE)
+test_y <- read_delim("Data/test/Y_test.txt", delim = " ", col_names = FALSE)
 y_data <- rbind(train_y, test_y)
 names(y_data) <- "Y"
 
 ## Read in test and train subject data, bind rows
-test_subjects <- read.csv("Data/test/subject_test.txt", sep = " ", header = FALSE)
-train_subjects <- read.csv("Data/train/subject_train.txt", sep = " ", header = FALSE)
+test_subjects <- read_delim("Data/test/subject_test.txt", delim = " ", col_names = FALSE)
+train_subjects <- read_delim("Data/train/subject_train.txt", delim = " ", col_names = FALSE)
 subjects <- rbind(train_subjects, test_subjects)
 
 ## Read in features and activity data
-features <- read.csv("Data/features.txt", sep = " ", header = FALSE)
-features_keep <- features$V2[grepl("mean\\(\\)|std", features$V2)] ## Keeping mean, std features
+features <- read_delim("Data/features.txt", delim = " ", col_names = FALSE)
+features_keep <- features$X2[grepl("mean\\(\\)|std", features$X2)] ## Keeping mean, std features
 
-activity <- read.csv("Data/activity_labels.txt", sep = " ", header = FALSE)
+activity <- read_delim("Data/activity_labels.txt", delim = " ", col_names = FALSE)
 
 
 ## Bind subject with x and y data
 data <- cbind(subjects, x_data, y_data)
 
 # Merge with activity data
-data <- merge(data, activity, by.x = "Y", by.y = "V1")
+data <- merge(data, activity, by.x = "Y", by.y = "X1")
 
 # Rename columns and select only relevant columns
-names(data) <- c("activityID", "subjectID", features$V2, "activity")
+names(data) <- c("activityID", "subjectID", features$X2, "activity")
 data <- data[c("subjectID", "activityID", "activity", features_keep)]
 
 # Make column names more readable
@@ -47,7 +48,7 @@ final_data <- data %>%
     mutate_at(4:69, as.numeric, digits = 10)
 
 ## Calculate mean of each variable for every combination of activity and subject
-data_summary <- final_data %>% 
+data_summary <- as_tibble(final_data) %>% 
     group_by(activity, subjectID) %>% 
     summarize_at(vars(timeBodyAccMeanX:freqBodyBodyGyroJerkMagMean), mean)
 
